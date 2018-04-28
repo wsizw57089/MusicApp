e0fdaf1ccd23a083fe7fbeed290c58606b62f765
W57089 <w57089@student.wsiz.rzeszow.pl>
2018-04-28 13:13:02 +0200

    Menu główne

--- app/src/main/java/com/example/stud/musicapp/MainActivity.java
+++ app/src/main/java/com/example/stud/musicapp/MainActivity.java
@@ -1,7 +1,10 @@
 package com.example.stud.musicapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button bTopSongs = findViewById(R.id.bTopSongs);
+        bTopSongs.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this,
+                        "TODO",
+                        Toast.LENGTH_SHORT).show();
+            }
+        });
+
     }
 }
