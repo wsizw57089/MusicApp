e6853fd111771922aefe87c7332a6042109a0d8a
W57089 <K@mikadze123>
2018-05-12 13:14:43 +0200

    Nowa aktywnosc

---
+++ app/src/main/java/com/example/stud/musicapp/api/apiClient.java
@@ -0,0 +1,5 @@
+package com.example.stud.musicapp.api;
+
+public interface apiClient {
+
+}
---
+++ app/src/main/java/com/example/stud/musicapp/api/apiService.java
@@ -0,0 +1,12 @@
+package com.example.stud.musicapp.api;
+
+import retrofit2.Retrofit;
+
+public class apiService {
+    public static apiClient getService(){
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://www.theaudiodb.com/api/v1/json/{1}/")
+                .build();
+        return retrofit.create(apiClient.class)
+    }
+}
